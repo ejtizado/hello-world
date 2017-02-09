45b2f218108f4b7796807bdb2f3ce199c2163ad9
E. Jorge Tizado <ej.tizado@unileon.es>
2017-02-09 09:44:58 +0100

    Update

--- model_make_dat_v3.py
+++ model_make_dat_v3.py
@@ -8,6 +8,7 @@ sys.path += ['/home/jorge/mounts/usb1/Research/proyectos/Sweden/TRASE/PCS/PYTHON
 # -----
 from collections import defaultdict
 
+<<<<<<< HEAD
 def consolidate_last( lst ):
     n = len(lst[0]) - 1
     rst = [ defaultdict(float) for i in range(n) ]
@@ -21,6 +22,21 @@ def consolidate_last_simple( lst ):
     for (k,v) in lst:
             rst[k] += v
     return rst
+=======
+def sum_item2( lst ):
+    ls1 = defaultdict(float)
+    for (k,v) in lst:
+        ls1[k] += v
+    return ls1
+
+def sum_item3( lst ):
+    ls1 = defaultdict(float)
+    ls2 = defaultdict(float)
+    for (k,m,v) in lst:
+        ls1[k] += v
+        ls2[m] += v
+    return ls1,ls2
+>>>>>>> 0c3e4d7450a6b5250ec863fae48598ebad75831d
 
 # -----
 from pcs import *
@@ -30,11 +46,14 @@ COUNTRY = 'BRAZIL'
 # limit is set for testing purposes, to load full datasets, set to None
 limit = None
 
+<<<<<<< HEAD
 filedat = "../../lp_2017/lp_hubs_O3.dat"
 comment ="\n# without Amazonas state (id: 385)\n# with limit ratio to 1.0\n\n"
 # SIN AMAZONAS ##
 print("WARNING: WITHOUT AMAZONAS IN LOGISTUC HUBS >>> %s" % filedat)
 
+=======
+>>>>>>> 0c3e4d7450a6b5250ec863fae48598ebad75831d
 # ---------------------------------------------------------
 # --- Exportation from logistic hub and from state
 print "Extracting HUBS data..."
@@ -43,6 +62,7 @@ chain = load_supply_chain( get_ref_id('COP before indicators'), limit=limit )
 # st_hubs = [node for (node,) in chain.get_nodes(1)]
 # st_hubs = {state.main_id: chain.get_subchain(state).get_sum() for state in st_hubs}
 #
+<<<<<<< HEAD
 # hubs = [node for (node,) in chain.get_nodes(2)]
 # hubs = {hub.main_id: chain.get_subchain(hub).get_sum() for hub in hubs}
 # >>>
@@ -54,6 +74,14 @@ st_hall = consolidate_last(st_hall)
 # >>> real : rdata = [ (flow.path[2].main_id, flow.raw_vol) for flow in chain.flows ]
 hdata = [ (flow.path[1].main_id, flow.path[2].main_id, flow.raw_vol) for flow in chain.flows if flow.path[2].name[:7] != 'UNKNOWN' and flow.path[1].main_id != 385 ]
 st_hubs, hubs = consolidate_last(hdata)
+=======
+# hubs = [node for (node,) in chain.get_nodes(1)]
+# hubs = {hub.main_id: chain.get_subchain(hub).get_sum() for hub in hubs}
+# >>>
+# >>> real total : rdata = [ (flow.path[2].main_id, flow.path[1].main_id, flow.raw_vol) for flow in chain.flows ]
+hdata = [ (flow.path[2].main_id, flow.path[1].main_id, flow.raw_vol) for flow in chain.flows if flow.path[2].name[:7] != 'UNKNOWN' ]
+hubs, st_hubs = sum_item3(hdata)
+>>>>>>> 0c3e4d7450a6b5250ec863fae48598ebad75831d
 # >>>
 cols_id = sorted(hubs.keys())
 # ---------------------------------------------------------
@@ -63,6 +91,7 @@ munic = get_data( get_ref_id('BRAZIL MUNICIPAL SOY PRODUCTION 2015'), limit=limi
 # >>>
 # st_muns = [node for (node,) in munic.get_nodes(1)]
 # st_muns = {state.main_id: prods.get_subchain(state).get_sum() for state in st_muns}
+<<<<<<< HEAD
 # >>> no tendria los datos de state of municipality <<<
 # muns = [node for (node,) in munic.get_nodes(0)]
 # muns = {mun.main_id: prods.get_subchain(mun).get_sum() for mun in muns}
@@ -79,6 +108,21 @@ for idm in st_muns:
 # >>> real total : xdata = [ (flow.path[0].main_id, flow.raw_vol) for flow in munic.flows ]
 mdata = [ (flow.path[0].main_id, flow.raw_vol * rat[flow.path[1].main_id]) for flow in munic.flows ]
 muns = consolidate_last(mdata)
+=======
+# >>> no tengo los datos de state of municipality <<<
+# muns = [node for (node,) in munic.get_nodes(1)]
+# muns = {mun.main_id: prods.get_subchain(mun).get_sum() for mun in muns}
+# >>>
+st_muns = [ (flow.path[1].main_id, flow.raw_vol) for flow in munic.flows ]
+st_muns = sum_item2(st_muns)
+ratio = {}
+for idm in st_muns:
+    if idm not in st_hubs: st_hubs[idm] = 0
+    ratio[idm] = st_hubs[idm] / st_muns[idm]
+## >> real total : xdata = [ (flow.path[0].main_id, flow.raw_vol) for flow in munic.flows ]
+mdata = [ (flow.path[0].main_id, flow.raw_vol * ratio[flow.path[1].main_id]) for flow in munic.flows ]
+muns = sum_item2(mdata)
+>>>>>>> 0c3e4d7450a6b5250ec863fae48598ebad75831d
 # >>>
 rows_id = sorted(muns.keys())
 # >>>
@@ -92,12 +136,20 @@ print "Extracting COST data..."
 nation_id = get_country_id(COUNTRY)
 cost = get_cost_list(nation_id, rows_id, cols_id, YEAR, mode='list')
 # ---------------------------------------------------------
+<<<<<<< HEAD
 # --- Make the data files to run GLPK Model
 nodes = max( max(rows_id), max(cols_id) )
 print "Writing data to ... "
 fout = open(filedat, 'w')
 fout.write("data;\n\n")
 fout.write(comment)
+=======
+# --- Make the data files for GLPK Model
+nodes = max( max(rows_id), max(cols_id) )
+print "Writing data..."
+fout = open("../lp_2017/lp_hubs_ratio.dat", 'w')
+fout.write("data;\n\n")
+>>>>>>> 0c3e4d7450a6b5250ec863fae48598ebad75831d
 fout.write("param n := " + str(nodes) + ";\n\n")
 fout.write("param : muns : src :=\n")
 for idm in muns:
